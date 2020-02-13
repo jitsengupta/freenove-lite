14ddd62f9a71ac1bcaebd615326a39bbd2f7101f
Arijit Sengupta <amdroid007@gmail.com>
2020-02-12 20:42:11 -0500

    Added a few things of my own and fixed some Motor and servo issues

---
+++ Code/Server/mytests/evdev-test.py
@@ -0,0 +1,8 @@
+from evdev import InputDevice, categorize, ecodes
+
+dev = InputDevice('/dev/input/event0');
+print(dev);
+for event in dev.read_loop():
+     if event.type == ecodes.EV_KEY:
+         print(categorize(event))
+	 print event.code
